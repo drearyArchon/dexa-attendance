7411f3ad4fd6f810b1e699181d8bdbb5f9cdf35d
drearyArchon <52845101+drearyArchon@users.noreply.github.com>
2026-09-05 00:08:59 +0700

    Uploaded MySQL Data

---
+++ MySQL Data/Image.sql
@@ -0,0 +1,25 @@
+/*
+-- Query: SELECT * FROM image
+LIMIT 0, 1000
+
+-- Date: 2026-09-05 00:07
+*/
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (43,1,'http://localhost:3100/img/TestIn.jpg','2026-09-04 16:21:34.000000','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (44,1,'http://localhost:3100/img/TestOut.jpg','2026-09-04 23:21:34.000000','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (45,2,'http://localhost:3100/img/TestIn.jpg','2026-09-04 16:21:34.000000','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (46,2,'http://localhost:3100/img/TestOut.jpg','2026-09-04 23:21:34.000000','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (47,3,'http://localhost:3100/img/TestIn.jpg','2026-09-04 16:21:34.000000','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (48,3,'http://localhost:3100/img/TestOut.jpg','2026-09-04 23:21:34.000000','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (49,4,'http://localhost:3100/img/TestIn.jpg','2026-09-04 16:21:34.000000','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (50,4,'http://localhost:3100/img/TestOut.jpg','2026-09-04 23:21:34.000000','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (51,5,'http://localhost:3100/img/TestIn.jpg','2026-09-04 16:21:34.000000','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (52,5,'http://localhost:3100/img/TestOut.jpg','2026-09-04 23:21:34.000000','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (53,6,'http://localhost:3100/img/TestIn.jpg','2026-09-04 16:21:34.000000','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (54,6,'http://localhost:3100/img/TestOut.jpg','2026-09-04 23:21:34.000000','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (55,1,'http://localhost:3100/img/1788540561520.jpg','2026-09-04 23:49:21.535834','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (56,1,'http://localhost:3100/img/1788540568793.jpg','2026-09-04 23:49:28.800330','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (57,1,'http://localhost:3100/img/1788540592340.jpg','2026-09-04 23:49:52.347370','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (58,1,'http://localhost:3100/img/1788540597792.jpg','2026-09-04 23:49:57.798637','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (59,10,'http://localhost:3100/img/1788540656579.jpg','2026-09-04 23:50:56.583992','out');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (60,11,'http://localhost:3100/img/1788540743629.jpg','2026-09-04 23:52:23.638536','in');
+INSERT INTO `` (`image_id`,`userUserId`,`url`,`timestamp`,`status`) VALUES (61,11,'http://localhost:3100/img/1788540749801.jpg','2026-09-04 23:52:29.807257','out');
---
+++ MySQL Data/User.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM user
+LIMIT 0, 1000
+
+-- Date: 2026-09-05 00:07
+*/
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (1,'admin','admin','admin','http://localhost:3100/img/DefaultProfile.jpg');
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (2,'ravid','password','user','http://localhost:3100/img/DefaultProfile.jpg');
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (3,'test','passtheword','user','http://localhost:3100/img/DefaultProfile.jpg');
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (4,'valiandi','wordpass','user','http://localhost:3100/img/DefaultProfile.jpg');
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (5,'taster','tastetest','user','http://localhost:3100/img/DefaultProfile.jpg');
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (6,'taster5','tastetest','user','http://localhost:3100/img/DefaultProfile.jpg');
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (10,'Taster7','tastethetester','user','http://localhost:3100/img/DefaultProfile.jpg');
+INSERT INTO `` (`user_id`,`username`,`password`,`role`,`profileImageUrl`) VALUES (11,'Tester8','tastetaste','user','http://localhost:3100/img/DefaultProfile.jpg');
